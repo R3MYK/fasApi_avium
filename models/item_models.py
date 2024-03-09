48d0bded7c5954eb38a4254931344d199a54f27b
Nicolás Martínez <remyblunt_@hotmail.com>
2024-03-09 18:18:43 -0300

    Creación de Router

--- models/item_models.py
+++ models/item_models.py
@@ -1,3 +1,4 @@
+"""
 from typing import Union
 
 from pydantic import BaseModel
@@ -6,6 +7,4 @@ from pydantic import BaseModel
 class Item(BaseModel):
     name : str
     price: float
-    is_offer: Union[bool, None] = None
-
-|
+    is_offer: Union[bool, None] = None 2"""
