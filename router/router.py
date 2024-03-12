2cc84dcb47a3f525cd82474637bf2d8a59203a1b
Nicolás Martínez <remyblunt_@hotmail.com>
2024-03-12 18:11:35 -0300

    Commit Creación PUT User

--- router/router.py
+++ router/router.py
@@ -9,7 +9,7 @@ from typing import List
 
 
 user = APIRouter()
-
+#GET
 @user.get("/")
 def root():
     return {"message": "Hi, I am Router from Router"}
@@ -20,7 +20,6 @@ def get_users():
         result = conn.execute(users.select()).fetchall()
         return result
     
-
 @user.get('/api/user/{user_cod}', response_model=UserSchema)
 def get_user(user_cod: int):
     with engine.connect() as conn:
@@ -28,6 +27,7 @@ def get_user(user_cod: int):
         return result
 
 
+#POST
 @user.post("/api/user", status_code=HTTP_201_CREATED)
 def create_user(data_user: UserSchema):
 
@@ -40,3 +40,17 @@ def create_user(data_user: UserSchema):
         return Response(status_code=HTTP_201_CREATED)
         #print (data_user)
         #print (new_user)
+    
+#PUT
+@user.put("api/user/{user_cod}")
+def update_user(data_uptade : UserSchema, user_cod = int):
+    print (data_uptade)
+    with engine.connect() as conn:
+        encrypt_pass = generate_password_hash(data_uptade.user_pass, "pdbkdf:sha256:30", 30)
+
+        conn.execute(users.update().values(user_pass = encrypt_pass, user_rut = data_uptade.user_rut, user_name = data_uptade.user_name,
+                                           user_last_name = data_uptade.user_last_name).where(users.c.user_cod == data_uptade.user_cod))
+        result = conn.execute(users.select().where(users.c.userd_cod == data_uptade.user_cod)).first
+
+        return result
+        
