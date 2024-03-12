77d9d83892c9f33edbc35eb68674bbf08371b504
Nicolás Martínez <remyblunt_@hotmail.com>
2024-03-12 18:13:04 -0300

    Comit Creación PUT Users 2

--- config/db.py
+++ config/db.py
@@ -3,6 +3,6 @@ from sqlalchemy import create_engine, MetaData
 #Url para conección con bases de datos
 engine = create_engine("mysql+pymysql://root@localhost:3306/avium")
 
-conn = engine.connect()
+#conn = engine.connect()
 
 metadata = MetaData()
--- router/router.py
+++ router/router.py
@@ -42,15 +42,23 @@ def create_user(data_user: UserSchema):
         #print (new_user)
     
 #PUT
-@user.put("api/user/{user_cod}")
-def update_user(data_uptade : UserSchema, user_cod = int):
-    print (data_uptade)
+@user.put("/api/user/{user_cod}", response_model=UserSchema) #Definir siempre responsse model
+
+def update_user(data_uptade: UserSchema, user_cod: int):
+    #print (data_uptade)
     with engine.connect() as conn:
-        encrypt_pass = generate_password_hash(data_uptade.user_pass, "pdbkdf:sha256:30", 30)
-
-        conn.execute(users.update().values(user_pass = encrypt_pass, user_rut = data_uptade.user_rut, user_name = data_uptade.user_name,
-                                           user_last_name = data_uptade.user_last_name).where(users.c.user_cod == data_uptade.user_cod))
-        result = conn.execute(users.select().where(users.c.userd_cod == data_uptade.user_cod)).first
-
+        #print (data_uptade)
+        
+        encrypt_pass = generate_password_hash(data_uptade.user_pass, "pbkdf2:sha256:30", 30)
+        #print (encrypt_pass)
+        conn.execute(users.update().values(user_pass=encrypt_pass, 
+                                           user_rut=data_uptade.user_rut, 
+                                           user_name=data_uptade.user_name, 
+                                           user_last_name=data_uptade.user_last_name, 
+                                           profile_cod=data_uptade.profile_cod).where(users.c.user_cod == user_cod))
+        result = conn.execute(users.select().where(users.c.user_cod == user_cod)).first()
+        conn.commit()
+        #conn.commit()
+       # print (result)
         return result
         
